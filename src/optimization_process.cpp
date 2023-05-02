e478449adc495a1e272c2e5301d28795befc2b06
mice85 <munoz.banon@gmail.com>
2023-05-02 11:14:44 +0200

    State propagation debugged.

--- src/optimization_process.cpp
+++ src/optimization_process.cpp
@@ -50,8 +50,8 @@ void OptimizationProcess::propagateState (Eigen::Matrix<double, 3, 1> p_a, Eigen
                                           Eigen::Matrix<double, 3, 1> p_b, Eigen::Quaternion<double> q_b, int id)
 {
     // Compute the relative transformation between the two frames.
-    Eigen::Quaternion<double> q_a_inverse = q_a.conjugate();
-    Eigen::Quaternion<double> q_ab_estimated = q_a_inverse * q_b;
+    //Eigen::Quaternion<double> q_a_inverse = q_a.conjugate();
+    Eigen::Quaternion<double> q_ab_estimated = q_b.inverse() * q_a;
 
     // Represent the displacement between the two frames in the A frame.
     Eigen::Matrix<double, 3, 1> p_ab_estimated = p_b - p_a;
@@ -61,7 +61,7 @@ void OptimizationProcess::propagateState (Eigen::Matrix<double, 3, 1> p_a, Eigen
     Pose3dWithCovariance propagated_pose;
     propagated_pose.id = id;
     propagated_pose.p = trajectory_estimated_.at(trajectory_estimated_.size()-1).p + p_ab_estimated;
-    propagated_pose.q = q_ab_estimated * trajectory_estimated_.at(trajectory_estimated_.size()-1).q;
+    propagated_pose.q = trajectory_estimated_.at(trajectory_estimated_.size()-1).q * q_ab_estimated;
     propagated_pose.covariance = trajectory_estimated_.at(trajectory_estimated_.size()-1).covariance;
 
     this->addPose3dToTrajectoryEstimated(propagated_pose);
